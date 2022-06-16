0382f1c5e67bd5aef4ca2f1ef2accecadfbb1a18
dimitar <dimitar.vukman@bluedropmedical.com>
2022-06-16 13:35:11 +0200

    Client WIP

---
+++ client/spring-boot-client/src/test/java/tech/dimitar/grpc/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dimitar.grpc.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

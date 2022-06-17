84cdcfec958dcf9f8d3472656375de5ae4dedf25
dimitar <dimitar.vukman@bluedropmedical.com>
2022-06-17 16:22:10 +0200

    gRPC service secured by OAUTH2 with Keycloak tokens.

--- server/spring-boot-server/src/main/java/tech/dimitar/service/grpc/demo/config/KeycloakJwtGrantedAuthoritiesConverter.java
+++ server/spring-boot-server/src/main/java/tech/dimitar/service/grpc/demo/config/KeycloakJwtGrantedAuthoritiesConverter.java
@@ -3,26 +3,21 @@ package tech.dimitar.service.grpc.demo.config;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import lombok.NoArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.core.log.LogMessage;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
 
+@NoArgsConstructor
 public final class KeycloakJwtGrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
-    public KeycloakJwtGrantedAuthoritiesConverter() {
-    }
 
     public Collection<GrantedAuthority> convert(Jwt jwt) {
         Collection<GrantedAuthority> grantedAuthorities = new ArrayList();
-        Iterator var3 = this.getAuthorities(jwt).iterator();
+        Iterator authorities = this.getAuthorities(jwt).iterator();
 
-        while(var3.hasNext()) {
-            String authority = (String)var3.next();
+        while(authorities.hasNext()) {
+            String authority = (String)authorities.next();
             grantedAuthorities.add(new SimpleGrantedAuthority(authority));
         }
 
@@ -38,9 +33,5 @@ public final class KeycloakJwtGrantedAuthoritiesConverter implements Converter<J
                     .map(String::new)
                     .collect(Collectors.toSet());
         return Collections.emptySet();
-    }
-
-    private Collection<String> castAuthoritiesToCollection(Object authorities) {
-        return (Collection)authorities;
     }
 }
